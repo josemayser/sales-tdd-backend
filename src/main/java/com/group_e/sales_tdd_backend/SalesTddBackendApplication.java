d87d5362b66d75aacebc3a2ed8c54a3ca45ba6bf
Jose Andres Mayser Vasquez <joseandresmayser@gmail.com>
2025-07-19 20:32:36 -0400

    First commit

---
+++ src/main/java/com/group_e/sales_tdd_backend/SalesTddBackendApplication.java
@@ -0,0 +1,13 @@
+package com.group_e.sales_tdd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTddBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesTddBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group_e/sales_tdd_backend/SalesTddBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_e.sales_tdd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTddBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
